960d5bed0cad2982dad2c1cea58dcbaa4ddeaa6d
suzzang_g <hyeonnee98@gmail.com>
2023-09-12 16:35:47 +0900

    Initial Commit

---
+++ myToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myToDoList
+//
+//  Created by 이수현 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
